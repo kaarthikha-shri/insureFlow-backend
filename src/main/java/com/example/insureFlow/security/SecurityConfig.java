86bcbd53d8bd7e294657e350ad6e601775905027
kaarthikha-shri <shriev3399@gmail.com>
2026-07-09 09:34:43 +0530

    Fix allowed origins in CORS configuration
    Removed trailing slash from allowed origin URL.

--- src/main/java/com/example/insureFlow/security/SecurityConfig.java
+++ src/main/java/com/example/insureFlow/security/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000","https://kaarthikha-shri.github.io/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000","https://kaarthikha-shri.github.io"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Cache-Control"));
         configuration.setExposedHeaders(List.of("Authorization"));
